f9865b391f4cea6d0c143783d074ffa3c44b8fbc
Yomna500 <yomnamanny2000@gmail.com>
2025-07-12 02:20:35 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,58 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// import Home from "./ui/Home";
+// import Error from "./ui/Error";
+// import Menu, { loader as menuLoader } from "./features/menu/Menu";
+// import Cart from "./features/cart/Cart";
+// import CreateOrder, {
+//   action as createOrderAction,
+// } from "./features/order/CreateOrder";
+// import Order, { loader as orderLoader } from "./features/order/Order";
+// import { action as updateOrderAction } from "./features/order/UpdateOrder";
+// import AppLayout from "./ui/AppLayout";
+
+// //create browser router
+// const router = createBrowserRouter([
+//   {
+//     element: <AppLayout />,
+//     errorElement: <Error />,
+//     children: [
+//       {
+//         path: "/", //root path
+//         element: <Home />, // the home page
+//       },
+//       {
+//         path: "/menu",
+//         element: <Menu />,
+//         loader: menuLoader, //provide data to menu page
+//         errorElement: <Error />,
+//       },
+//       {
+//         path: "/cart",
+//         element: <Cart />,
+//       },
+//       {
+//         path: "/order/new",
+//         element: <CreateOrder />,
+//         action: createOrderAction,
+//       },
+//       {
+//         path: "/order/:orderId",
+//         element: <Order />,
+//         loader: orderLoader,
+//         errorElement: <Error />,
+//         action: updateOrderAction,
+//       },
+//     ],
+//   },
+// ]);
+
+// function App() {
+//   return <RouterProvider router={router} />;
+// }
+
+// export default App;
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./ui/Home";
 import Error from "./ui/Error";
@@ -11,20 +65,19 @@ import Order, { loader as orderLoader } from "./features/order/Order";
 import { action as updateOrderAction } from "./features/order/UpdateOrder";
 import AppLayout from "./ui/AppLayout";
 
-//create browser router
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     element: <AppLayout />,
     errorElement: <Error />,
     children: [
       {
-        path: "/", //root path
-        element: <Home />, // the home page
+        path: "/",
+        element: <Home />,
       },
       {
         path: "/menu",
         element: <Menu />,
-        loader: menuLoader, //provide data to menu page
+        loader: menuLoader,
         errorElement: <Error />,
       },
       {
--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,11 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store";
-// import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <HashRouter> */}
       <App />
-      {/* </HashRouter> */}
     </Provider>
   </React.StrictMode>,
 );
