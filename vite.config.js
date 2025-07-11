caf74f8c885f6b42f3581d20de22dc73b173ac73
Yomna500 <yomnamanny2000@gmail.com>
2025-07-12 01:41:19 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// https://vitejs.dev/config/
+
 export default defineConfig({
+  base: "/pizza-app/", // اسم الـ repo بتاعك
   plugins: [react()],
 });
