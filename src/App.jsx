89c4ec3e127f7e64a9364ce862d6a98bfcf26778
Yomna500 <yomnamanny2000@gmail.com>
2025-07-12 02:26:21 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -52,56 +52,30 @@
 // }
 
 // export default App;
-import { createHashRouter, RouterProvider } from "react-router-dom";
-
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./ui/Home";
 import Error from "./ui/Error";
-import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Menu from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import CreateOrder, {
-  action as createOrderAction,
-} from "./features/order/CreateOrder";
-import Order, { loader as orderLoader } from "./features/order/Order";
-import { action as updateOrderAction } from "./features/order/UpdateOrder";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 
-const router = createHashRouter([
-  {
-    element: <AppLayout />,
-    errorElement: <Error />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/menu",
-        element: <Menu />,
-        loader: menuLoader,
-        errorElement: <Error />,
-      },
-      {
-        path: "/cart",
-        element: <Cart />,
-      },
-      {
-        path: "/order/new",
-        element: <CreateOrder />,
-        action: createOrderAction,
-      },
-      {
-        path: "/order/:orderId",
-        element: <Order />,
-        loader: orderLoader,
-        errorElement: <Error />,
-        action: updateOrderAction,
-      },
-    ],
-  },
-]);
-
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HashRouter>
+      <AppLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order/new" element={<CreateOrder />} />
+          <Route path="/order/:orderId" element={<Order />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </AppLayout>
+    </HashRouter>
+  );
 }
 
 export default App;
